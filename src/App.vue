c003645d3b96fcbcfde6e17bb68c15c1896532f7
evgeny <afazhenya2001@gmail.com>
2023-05-21 22:45:00 +0300

    fix lint

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div>App</div>
+  <div></div>
+  <!--  <div><TestComponent /></div>-->
 </template>
+<script setup lang="ts">
+// import TestComponent from "./components/TestComponent.vue";
+</script>
 
 <style scoped></style>
