6843908b603b918e63d838e9659a2870efe14d00
evgeny <afazhenya2001@gmail.com>
2023-05-26 16:05:09 +0300

    fix app.vue and package.json

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div></div>
-  <!--  <div><TestComponent /></div>-->
 </template>
 
-<script setup lang="ts">
-// import TestComponent from "./components/TestComponent.vue";
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
