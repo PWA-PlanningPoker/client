ed29fa3108a6b11b23059ae539b321181cc3d9cd
evgeny <afazhenya2001@gmail.com>
2023-05-26 18:16:28 +0300

    config fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@ui": path.resolve(__dirname, "src/ui"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@containers": path.resolve(__dirname, "src/containers"),
+      "@components": path.resolve(__dirname, "src/components"),
+    },
+  },
   plugins: [vue()],
-})
+});
